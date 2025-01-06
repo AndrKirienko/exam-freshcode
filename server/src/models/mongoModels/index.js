f5435b7e5fb0b1c392ab8b58c9e1b87619e0bb5f
ndrw <ndrw.iieo@gmail.com>
2025-01-06 16:20:24 +0200

    Refactor mongo connection

--- server/src/models/mongoModels/index.js
+++ server/src/models/mongoModels/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 // ===============================
 const env = process.env.NODE_ENV || 'development';
-const configPath = path.join(__dirname, '..', 'config/mongoConfig.json');
+const configPath = path.join(__dirname, '../..', '/config/mongoConfig.json');
 const config = require(configPath)[env];
 
 /* mongoose.connect(
@@ -23,4 +23,10 @@ mongoose
 
 mongoose.set('debug', env === 'development');
 
-module.exports = mongoose;
+const db = {};
+db.Catalog = require('./Catalog');
+db.Conversation = require('./Conversation');
+db.Message = require('./Message');
+db.mongoose = mongoose;
+
+module.exports = db;
