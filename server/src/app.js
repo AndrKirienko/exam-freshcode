f2938e05c6f4c1eac52f7b58673a1a27f05d6b42
ndrw <ndrw.iieo@gmail.com>
2025-01-06 15:00:31 +0200

    Refactor server index

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const cors = require('cors');
+const express = require('express');
+const router = require('./router');
+const handlerError = require('./handlerError/handler');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/public', express.static('public'));
+app.use(router);
+app.use(handlerError);
+
+module.exports = app;
