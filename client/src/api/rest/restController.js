135d753c3af530860024a03b2cbed0c905bbd204
ndrw <ndrw.iieo@gmail.com>
2025-01-10 17:38:54 +0200

    Refactor RestController: group methods by blocks

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -3,23 +3,21 @@ import http from '../interceptor';
 
 export const registerRequest = data => http.post('registration', data);
 export const loginRequest = data => http.post('login', data);
+
 export const getUser = () => http.post('getUser');
+export const updateUser = data => http.post('updateUser', data);
+
 export const updateContest = data =>
   http.patch(`contests/${data.get('contestId')}`, data);
-export const setNewOffer = data => http.post('setNewOffer', data);
-export const setOfferStatus = data => http.post('setOfferStatus', data);
-export const downloadContestFile = data =>
-  http.get(`downloadFile/${data.fileName}`);
+export const getCustomersContests = data =>
+  http.get(`contests/byCustomer?${stringify(data)}`);
+export const getActiveContests = data =>
+  http.get(`contests?${stringify(data)}`);
+export const getContestById = ({ contestId }) =>
+  http.get(`contests/${contestId}`);
 export const payMent = data => http.post('contests', data.formData);
-export const changeMark = data => http.post('changeMark', data);
-export const getPreviewChat = () => http.post('getPreview');
-export const getDialog = data => http.post('getChat', data);
 export const dataForContest = data => http.post('dataForContest', data);
-export const cashOut = data => http.post('cashout', data);
-export const updateUser = data => http.post('updateUser', data);
-export const newMessage = data => http.post('newMessage', data);
-export const changeChatFavorite = data => http.post('favorite', data);
-export const changeChatBlock = data => http.post('blackList', data);
+
 export const getCatalogList = data => http.get('catalogs', data);
 export const addChatToCatalog = data => http.post('addNewChatToCatalog', data);
 export const createCatalog = data => http.post('createCatalog', data);
@@ -27,11 +25,22 @@ export const deleteCatalog = data => http.post('deleteCatalog', data);
 export const removeChatFromCatalog = data =>
   http.post('removeChatFromCatalog', data);
 export const changeCatalogName = data => http.post('updateNameCatalog', data);
-export const getCustomersContests = data =>
-  http.get(`contests/byCustomer?${stringify(data)}`);
 
-export const getActiveContests = data =>
-  http.get(`contests?${stringify(data)}`);
 
-export const getContestById = ({ contestId }) =>
-  http.get(`contests/${contestId}`);
+
+export const setNewOffer = data => http.post('setNewOffer', data);
+export const setOfferStatus = data => http.post('setOfferStatus', data);
+
+export const downloadContestFile = data =>
+	http.get(`downloadFile/${data.fileName}`);
+	
+export const changeMark = data => http.post('changeMark', data);
+
+export const getPreviewChat = () => http.post('getPreview');
+
+export const cashOut = data => http.post('cashout', data);
+
+export const getDialog = data => http.post('getChat', data);
+export const newMessage = data => http.post('newMessage', data);
+export const changeChatFavorite = data => http.post('favorite', data);
+export const changeChatBlock = data => http.post('blackList', data);
