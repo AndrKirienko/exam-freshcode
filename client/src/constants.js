ee6c98583bd21533031aacb4ade0ed4a9fa10604
ndrw <ndrw.iieo@gmail.com>
2025-01-18 17:01:09 +0200

    Fix mainFooter

--- client/src/constants.js
+++ client/src/constants.js
@@ -48,40 +48,4 @@ export default {
     'a Product',
     'a Startup',
   ],
-  FooterItems: [
-    {
-      title: 'SQUADHELP',
-      items: ['About', 'Contact', 'How It Works?', 'Testimonials', 'Our Work'],
-    },
-    {
-      title: 'RESOURCES',
-      items: [
-        'How It Works',
-        'Become a Creative',
-        'Business Name Generator',
-        'Discussion Forum',
-        'Blog',
-        'Download eBook',
-        'Pricing',
-        'Help & FAQs',
-      ],
-    },
-    {
-      title: 'OUR SERVICES',
-      items: [
-        'Naming',
-        'Logo Design',
-        'Taglines',
-        'Premium Names For Sale',
-        'Creative Owned Names For Sale',
-        'Audience Testing',
-        'Trademark Research & Filling',
-        'Managed Agency Service',
-      ],
-    },
-    {
-      title: 'LEGAL',
-      items: ['Terms of Service', 'Privacy Policy', 'Cookie Policy'],
-    },
-  ],
 };
