f63165babd1c040302c1fa0d4741c4a8a9830d1b
ndrw <ndrw.iieo@gmail.com>
2025-01-26 19:37:16 +0200

    Add searchForm

--- client/src/components/HowItWorks/SearchForm/SearchForm.jsx
+++ client/src/components/HowItWorks/SearchForm/SearchForm.jsx
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './SearchForm.module.sass';
 import searchData from './../../../data/howItWorksData/searchData.json';
+import searchIcon from '../../../../public/staticImages/howItWorksImg/search_icon/search_icon';
 
 class SearchForm extends Component {
   render () {
     return (
       <>
-        <div></div>
+        <form className={styles.searchContainer} action='#' method='GET'>
+          <div className={styles.icon}>{searchIcon}</div>
+          <input
+            name='search'
+            type='text'
+            placeholder='Search Over 200,000 + Premium Names'
+            required
+            className={styles.searchInput}
+          />
+          <button className={styles.searchBtn} type='submit'>
+            {searchIcon}
+          </button>
+        </form>
         <ul className={styles.listTag}>
           {searchData.map((key, index) => (
             <li key={index}>
