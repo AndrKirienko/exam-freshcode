f644eead0fc59b385371f7bcfdedc14d5ac01bae
ndrw <ndrw.iieo@gmail.com>
2025-01-28 16:21:28 +0200

    Add initial list events

--- client/src/components/Events/EventsList/EventsList.jsx
+++ client/src/components/Events/EventsList/EventsList.jsx
@@ -1,7 +1,58 @@
 import React, { Component } from 'react';
+import { calculateTimers } from '../../../utils/calculateTimers';
+
 class EventsList extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      events: [],
+      timers: {},
+    };
+  }
+
+  componentDidMount () {
+    const storedEvents = JSON.parse(localStorage.getItem('events')) || [];
+
+    const sortedEvents = storedEvents.sort((current, next) => {
+      const dateCurrent = new Date(current.datetime);
+      const dateNext = new Date(next.datetime);
+      return dateCurrent - dateNext;
+    });
+
+    this.setState({ events: sortedEvents });
+    this.timerInterval = setInterval(this.updateTimers, 1000);
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timerInterval);
+  }
+
+  updateTimers = () => {
+    const { events } = this.state;
+    const currentTimers = calculateTimers(events);
+    this.setState({ timers: currentTimers });
+  };
+
   render () {
-    return <>list</>;
+    const { events, timers } = this.state;
+
+    return (
+      <div>
+        <h2>Events List</h2>
+        {events.length > 0 ? (
+          <ul>
+            {events.map((event, index) => (
+              <li key={index}>
+                <strong>{event.eventName}</strong> - {event.datetime}
+                <span>{timers[index]}</span>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No events created yet.</p>
+        )}
+      </div>
+    );
   }
 }
 
---
+++ client/src/utils/calculateTimers.js
@@ -0,0 +1,38 @@
+/**
+ * Calculates the remaining time for an array of events
+ * @param {Array} events - Array of events with date and time fields
+ * @returns {Object} - Object with event indexes and time remaining
+ */
+
+export const calculateTimers = events => {
+  const currentTimers = {};
+
+  events.forEach((event, index) => {
+    const eventTime = new Date(`${event.datetime}`).getTime();
+    const now = Date.now();
+    const timeDiff = eventTime - now;
+
+    if (timeDiff > 0) {
+      const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+      if (days > 0) {
+        currentTimers[index] = `${days}d`;
+      } else if (hours > 0) {
+        currentTimers[index] = `${hours}h`;
+      } else if (minutes > 0) {
+        currentTimers[index] = `${minutes}m`;
+      } else {
+        currentTimers[index] = `${seconds}s`;
+      }
+    } else {
+      currentTimers[index] = 'Event has ended';
+    }
+  });
+
+  return currentTimers;
+};
