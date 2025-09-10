a44b2e127e5d0bb50fea52ed5d2f1ca48d74ac30
ndrw <ndrw.iieo@gmail.com>
2025-09-10 12:07:02 +0300

    Refact path to postgresConfig

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -4,19 +4,12 @@ const Sequelize = require('sequelize');
 const { logError } = require('../utils/logger');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const configPath =
-  env === 'production'
-    ? path.join(
-        __dirname,
-        '..',
-        '..',
-        '..',
-        'src',
-        'server',
-        'config',
-        'postgresConfig.json'
-      )
-    : path.join(__dirname, '..', 'config', 'postgresConfig.json');
+const configPath = path.resolve(
+  __dirname,
+  '..',
+  'config',
+  'postgresConfig.json'
+);
 const config = require(configPath)[env];
 const db = {};
 
