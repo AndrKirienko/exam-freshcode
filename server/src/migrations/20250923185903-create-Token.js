d2613aacf1ffe53b0ebbd4031a1354a8bcf0d8b0
ndrw <ndrw.iieo@gmail.com>
2025-09-23 22:53:00 +0300

    Add refreshToken to DB

---
+++ server/src/migrations/20250923185903-create-Token.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      refreshToken: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tokens');
+  },
+};
